c1aeb00b65db026477116bade88d550b609b1765
GitHub Action <action@github.com>
2022-10-29 00:06:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/artronics/ideavimflow/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/artronics/ideavimflow/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.artronics.ideavimflow.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.artronics.ideavimflow.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/artronics/ideavimflow/services/MyProjectService.kt
+++ src/main/kotlin/com/github/artronics/ideavimflow/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.artronics.ideavimflow.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.artronics.ideavimflow.MyBundle
 
 class MyProjectService(project: Project) {
 
